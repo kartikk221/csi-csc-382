167afa2b82028c571d6cb899fbc36f0f28286adb
kartikk221 <kartikk221@yahoo.com>
2022-04-12 17:09:48 -0400

    Completed Project 3

--- project-2-monte-carlo/kumar-project-2.cpp
+++ project-2-monte-carlo/kumar-project-2.cpp
@@ -6,19 +6,16 @@
  * algorithm.
  */
 
+#include <climits>
 #include <iomanip>
 #include <iostream>
 #include <random>
-#include <time.h>
+#include <string>
+
 using namespace std;
 
-// Generates a random integer between provided min and max values
-int random_integer(int min, int max) {
-  // Generate a random number between min and max
-  return min + rand() % (max - min + 1);
-}
-
 // Algorithm taken from project direction sheet
+// Linear Search Algorithm
 int A5_Find(int x, int A[], int n) {
   int j;
   for (j = 0; j < n; j++) {
@@ -33,55 +30,55 @@ const int n = 50; // 1.a) Let n = 50
 const int iterations = 10000;
 
 void perform_average_scenarios(int bound) {
-  // 1) Calculated Average
   // 1.c) Let x be an integer between 0 and bound.
-  int x = random_integer(0, bound);
+  int x = rand() % bound;
 
   // 1.d) Let hits = 0.
   int hits = 0;
   int total_steps = 0;
-
-  // 1.g) Repeat steps e and f 10,000 times (in other words create 10,000
-  // sequences of 50 integers each. Clearly Sequences is a 50 x 10,000
-  // two-dimensional array
-  int Sequences[iterations][n];
   for (int i = 0; i < iterations; i++) {
+    int Sequence[n];
+
     // 1.e) Generate a sequence of n integers using a random number generator
     // where the numbers of the sequence are between 0 and bound.
     for (int j = 0; j < n; j++) {
-      Sequences[i][j] = random_integer(0, bound);
+      Sequence[j] = rand() % bound;
     }
 
     // 1.f) If x is equal to any of the numbers generated in the sequence
     // increment hits by one (if x appears more than once do not increment hits
     // every time).
-    for (int j = 0; j < n; j++) {
-      bool is_double = Sequences[i][j] == Sequences[i][j + 1];
-      if (!is_double && Sequences[i][j] == x) {
-        hits++;
-      }
+    int steps_taken = A5_Find(x, Sequence, n);
+    if (steps_taken > 0) {
+      // Count the number of steps this successul search took
+      total_steps += steps_taken;
+      hits++;
+    } else {
+      // Count the number of steps this unsuccessful search took
+      total_steps += n;
     }
-
-    // Calculate the total steps
-    total_steps += A5_Find(x, Sequences[i], n);
   }
 
   // 1.h) Let q = hits / 10,000.
-  double q = hits / iterations;
+  // This is the quality ratio of the of random arrays to the ideal scenario
+  // where all 10k arrays have x somewhere in them
+  double q = hits / (double)iterations;
 
-  // 1.i) Let calculated_average -> AI(n) = ((h^2)/4) + (3/4)(n) where h is
-  // bound
-  double calculated_average = ((bound * bound) / 4) + (3 * n / 4);
+  // 1.i) Let calculated_average -> A(n) = (n + 1) / 2 -> Real[A(n)] = A(n) * q
+  // where q is the quality of the random We only want to add one term if it is
+  // less then the max integer value possible to prevent overflow
+  double calculated_average = static_cast<double>(n + (q / 2) - (q * n) / 2);
 
-  // Calculate the real average
-  double real_average = total_steps / iterations;
+  // Calculate the real average by taking the total steps and dividing by the
+  // number of iterations
+  double real_average = static_cast<double>(total_steps) / iterations;
 
   // Set the print number decimal precision to 2 decimal places
   cout << setprecision(2) << fixed;
 
   // Print the results for this bound
-  cout << bound << setw(30) << calculated_average << setw(30) << real_average
-       << endl;
+  cout << (bound == INT_MAX ? "Infinity" : to_string(bound)) << setw(30)
+       << calculated_average << setw(30) << real_average << endl;
 }
 
 int main() {
@@ -97,7 +94,7 @@ int main() {
   srand(time(NULL));
 
   // Run steps 1-2 for the following bound values
-  int bounds[] = {30, 50, 80, 100, 1000, 10000, INFINITY};
+  int bounds[] = {30, 50, 80, 100, 1000, 10000, INT_MAX};
   for (int i = 0; i < *(&bounds + 1) - bounds; i++) {
     perform_average_scenarios(bounds[i]);
   }
---
+++ project-3-insertion-sort/kumar-project-3.cpp
@@ -0,0 +1,86 @@
+/**
+ * @file kumar-project-3.cpp
+ * @author Kartik Kumar
+ * @brief Project 3 - In this project we will try to match the Average Case of
+ * algorithm A10 (Insertion Sort) as we derived in class and the “Real Average”
+ * of the algorithm using Monte Carlo approach.
+ */
+
+#include <climits>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <string>
+
+using namespace std;
+
+int InsertionMod(int A[], int n) {
+  int steps = 0;
+  int i, j, temp;
+  A[0] =
+      -32768; // smallest possible integer using 2 bytes integer representation
+  for (i = 1; i <= n; i++) {
+    j = i;
+    while (A[j] < A[j - 1]) { // swap
+      temp = A[j];
+      A[j] = A[j - 1];
+      A[j - 1] = temp;
+      j--;
+      steps++;
+    }
+    steps++;
+  }
+  return steps;
+}
+
+const int n = 100; // 1.a) Let n = 50
+const int iterations = 100000;
+
+void perform_average_scenarios(int bound) {
+  int total_num_steps = 0;
+  for (int i = 0; i < iterations; i++) {
+    int Sequence[n];
+    for (int j = 0; j < n; j++) {
+      Sequence[j] = rand() % bound;
+      Sequence[j] *= rand() > RAND_MAX / 2 ? 1 : -1;
+    }
+
+    // Count the number of steps this insertion sort took
+    total_num_steps += InsertionMod(Sequence, n);
+  }
+
+  // Calculate the expected number of steps for insertion sort
+  double calculated_average = (pow(n, 2) / 4) + (3 * n / 4);
+
+  // Calculate the real average number of steps by dividing the total number of
+  // steps by the number of iterations
+  double real_average = (double)total_num_steps / iterations;
+
+  // Set the print number decimal precision to 2 decimal places
+  cout << setprecision(2) << fixed;
+
+  // Print the results for this bound
+  cout << (bound == INT_MAX ? "Infinity" : to_string(bound)) << setw(30)
+       << calculated_average << setw(30) << real_average << endl;
+}
+
+int main() {
+  // Log the information columns with a border line
+  cout << "Bound" << setw(30) << "Calculated Average" << setw(30)
+       << "Real Average" << endl;
+
+  // Print the border line
+  cout << "-----------------------------------------------------------------"
+       << endl;
+
+  // Seed the random number generator
+  srand(time(NULL));
+
+  // Run steps 1-2 for the following bounds
+  int bounds[] = {100, 500, 1000, 2500, 3000, 3500};
+  for (int i = 0; i < *(&bounds + 1) - bounds; i++) {
+    perform_average_scenarios(bounds[i]);
+  }
+
+  return 0;
+}
